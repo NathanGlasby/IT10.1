6999036ac8237be74d72d33d01bd5ca9dc043bc7
natha <nathan.glasby@gmail.com>
2026-02-03 23:26:24 +0200

    Refactor CharacterBuilding.java: streamline code and enhance clarity
    - Removed unnecessary comments and cleaned up the main method.
    - Added comments to clarify the Unicode values for characters.
    - Improved code readability by organizing variable declarations and print statements.

--- CharacterBuilding/src/main/java/com/mycompany/characterbuilding/CharacterBuilding.java
+++ CharacterBuilding/src/main/java/com/mycompany/characterbuilding/CharacterBuilding.java
@@ -11,22 +11,17 @@ package com.mycompany.characterbuilding;
 public class CharacterBuilding {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-// please fix all code below        
+        char letter = 'a';
+        System.out.println(letter);
 
-// char letter = 'a';
-        // System.out.println(letter);
-        
-        char letter2 = 98;
+        char letter2 = 98;  // 98 is the Unicode value for 'b'
         System.out.println(letter2);
-        
-        //char letter3 = 'A';
-        //System.out.println(letter);
-        
-        //char letter4 = 49;
-        //System.out.println(letter2);
-        //System.out.print(i + " ");
+
+        char letter3 = 'A';
+        System.out.println(letter3);
+
+        char letter4 = 49;  // 49 is the Unicode value for '1'
+        System.out.println(letter4);
     }
-  }
+}
 
