437c65b8de2afb68e4d44ce4286ff5b75754c3dd
natha <nathan.glasby@gmail.com>
2026-02-03 12:45:49 +0200

    commit

---
+++ CharacterBuilding/src/main/java/com/mycompany/characterbuilding/CharacterBuilding.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.characterbuilding;
+
+/**
+ *
+ * @author natha
+ */
+public class CharacterBuilding {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+// please fix all code below        
+
+// char letter = 'a';
+        // System.out.println(letter);
+        
+        char letter2 = 98;
+        System.out.println(letter2);
+        
+        //char letter3 = 'A';
+        //System.out.println(letter);
+        
+        //char letter4 = 49;
+        //System.out.println(letter2);
+        //System.out.print(i + " ");
+    }
+  }
+
