8e15ca54a5370a07238ea610b0f0925c3e04eded
natha <nathan.glasby@gmail.com>
2026-02-06 12:47:46 +0200

    UserInput and JavaMaths lesson

---
+++ JavaMaths/src/javamaths/JavaMaths.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javamaths;
+
+/**
+ *
+ * @author natha
+ */
+public class JavaMaths {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(1 + 1);
+        System.out.println(5 - 2);
+        System.out.println(3 * 3);
+        System.out.println(8 / 3); // integer devision (cuts off decimal part of answer no matter what, doesnt round up or down)
+        
+        System.out.println(5 / 2); // integer devision (RESULT: 2)
+        System.out.println(5.0 / 2.0); // double devision (RESULT: 2.5)
+        
+        System.out.println(5 % 2); // remainder (modulus)
+        
+        
+        
+    }
+    
+}
